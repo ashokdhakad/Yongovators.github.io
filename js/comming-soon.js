7ba10729714bdca4f510a41caaa0ba813d64cfcb
ashokdhakad <aashudhakad0@gmail.com>
2021-10-31 02:59:00 +0530

    update pages

--- js/comming-soon.js
+++ js/comming-soon.js
@@ -3,9 +3,9 @@
         function getTimeRemaining(endtime) {
             var t = Date.parse(endtime) - Date.parse(new Date());
             var seconds = Math.floor((t / 1000) % 60);
-            var minutes = Math.floor((t / 1000 / 60) % 60);
-            var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-            var days = Math.floor(t / (1000 * 60 * 60 * 24));
+            var minutes = Math.floor((t + 1 / 1000 / 60) % 60);
+            var hours = Math.floor((t + 0.15 / (1000 * 60 * 60)) % 24);
+            var days = Math.floor(t  / (1000 * 60 * 60 * 24 ));
             return {
                 'total': t,
                 'days': days,
